37b3046358501369b12f8fd19db7571cded5f3a4
Haroro <haroro@HarorodeMacBook-Pro.local>
2022-03-17 15:49:53 +0800

    feat:修改代码块

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,32 @@
+const base = require('./webpack.config')
+const path = require('path')
+const { merge } = require('webpack-merge')
+const WebpackBar = require('webpackbar')
+
+module.exports = merge(base, {
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    hot: true, // 热更新,
+    port: 'auto',
+    client: {
+      logging: 'none',
+      progress: true,
+      overlay: {
+        errors: false,
+        warnings: false,
+      },
+    },
+    static: {
+      directory: path.join(__dirname, '../public'),
+    },
+  },
+  plugins: [
+    new WebpackBar(),
+  ],
+  stats: {
+    assets: false,
+    modules: false,
+    children: false,
+  },
+})
